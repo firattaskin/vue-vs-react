42f3bf1cd3260878bcf243443d6e92d1e26d89b2
firattaskin <firattaskin93@gmail.com>
2021-10-25 21:13:06 +0300

    developers from parent react

--- kz_react/src/App.js
+++ kz_react/src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import AddDeveloper from './components/AddDeveloper.js';
 import ListDeveloper from './components/ListDeveloper.js';
+import {useState,useEffect} from 'react';
 
 
 function App() {
+  const [developers,setDevelopers] = useState([]);
+
+  useEffect(() => {
+      setTimeout(()=>{
+          fetch('http://localhost:3002/developers')
+          .then(res => res.json())
+          .then(data => setDevelopers(data))
+          .catch(err => console.log(err.message));
+      },1000);
+  },[]);
+
   return (
     <div id="app">
       <h1>React App</h1>
       <AddDeveloper />
-      <ListDeveloper />
+      <ListDeveloper developers={developers} setDevelopers={setDevelopers} />
     </div>
   );
 }
--- kz_react/src/components/ListDeveloper.js
+++ kz_react/src/components/ListDeveloper.js
@@ -1,22 +1,11 @@
 
 import SingleDeveloper from "./SingleDeveloper";
-import {useState,useEffect} from 'react';
 
-function ListDeveloper() {
-    const [developers,setDevelopers] = useState([]);
-
-    useEffect(() => {
-        setTimeout(()=>{
-            fetch('http://localhost:3002/developers')
-            .then(res => res.json())
-            .then(data => setDevelopers(data))
-            .catch(err => console.log(err.message));
-        },1000);
-    },[]);
+function ListDeveloper(props) {
 
     return (
     <div>
-        { developers.length ? developers.map((developer)=><SingleDeveloper key={developer.id} developer={developer} />) : 'Loading..' }
+        { props.developers.length ? props.developers.map((developer)=><SingleDeveloper key={developer.id} developer={developer} />) : 'Loading..' }
     </div>
     );
 }
